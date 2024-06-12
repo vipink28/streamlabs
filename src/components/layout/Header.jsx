db5da98578715369de206079b4fdb5df32268ce7
vipink28 <vipinkalyan28@gmail.com>
2024-06-12 10:29:50 +0530

    header data fetched.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { IMG_URL } from '../../helper/apirequests';
 
 function Header(props) {
+    const { video } = props;
     return (
         <div>
-
+            <img src={IMG_URL + video.backdrop_path} alt="" />
+            <div className=''>
+                <h2>{video.name || video.original_name || video.title || video.original_title}</h2>
+                <p>{video.overview}</p>
+            </div>
         </div>
     );
 }
--- src/pages/Homescreen.jsx
+++ src/pages/Homescreen.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNetflixOriginals, netflixOrignalsSelector } from '../features/tv/tvSlice';
+import Header from '../components/layout/Header';
 
 function Homescreen(props) {
-    const netflix = useSelector(netflixOrignalsSelector);
+    const { data, status, error } = useSelector(netflixOrignalsSelector);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchNetflixOriginals());
     }, [])
 
-    console.log(netflix);
-
     return (
-        <div>
-            Homescreen
-        </div>
+        <>
+            {
+                status === "success" ?
+                    < Header video={data.results[Math.floor(Math.random() * data.results.length)]} />
+                    : ""
+            }
+        </>
     );
 }
 
